c4fa43dc17feeb54459b3891f647db0fea95c916
Marcos Lucas <marcosvaldeni@gmail.com>
2021-03-29 21:34:32 +0100

    navigation's menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -7,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ultra.news" />
         <nav>
-          <a>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Home</a>
+          </Link>
         </nav>
 
         <SignInButton />
