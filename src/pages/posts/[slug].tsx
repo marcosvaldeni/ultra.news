2b9c9de57f4eff516fd71d4ce4b24df5617c1f99
Marcos Lucas <marcosvaldeni@gmail.com>
2021-04-05 19:11:08 +0100

    vallinding active subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,16 @@ export default function Post({post}: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req });
   const { slug } = params;
+
   
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
@@ -58,9 +67,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
       year: 'numeric'
     })
   };
-
-
-  
+ 
 
   return {
     props: {
